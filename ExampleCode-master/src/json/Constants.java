21aa68ae9447e36ce1f8f06b79c73dbca7f009e3
willy1087 <willy1087@gmail.com>
2015-05-23 10:23:26 -0700

    Robotic arm module
    This is the robotic arm test

---
+++ ExampleCode-master/src/json/Constants.java
@@ -0,0 +1,16 @@
+package json;
+
+/*	
+ * Created by: 	Jonathan Young
+ * Date: 		May 14, 2015
+ */
+
+public final class Constants {
+	public static final int ONE = 1;
+	public static final int TWO = 16;
+	public static final int THREE = 2;
+	
+	public static final int PORT_ONE = 9013;
+	public static final int PORT_TWO = 9016;
+	
+}
