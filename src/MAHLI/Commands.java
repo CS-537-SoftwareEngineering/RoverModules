8b01f5017215181e6a7e06cf2397616bbc534e0e
shreyasDarwhatkar <shreyas.darwhatkar@hotmail.com>
2015-05-23 14:01:20 -0700

    commands added

---
+++ src/MAHLI/Commands.java
@@ -0,0 +1,15 @@
+package MAHLI;
+
+public class Commands {
+	public static String MAHLI_Camera_ON;
+	public static String MAHLI_Camera_OFF;
+	public static String MAHLI_NightIllumination_ON;
+	public static String MAHLI_NightIllumination_OFF;
+	public static String MAHLI_AutoFocus_ON;
+	public static String MAHLI_AutoFocus_OFF;
+	public static String MAHLI_Video_ON;
+	public static String MAHLI_Video_OFF;
+	public static String MAHLI_Image_Capture;
+	public static String MAHLI_Image_store;
+	public static String MAHLI_Image_read;
+}
