25b58723445ac66d37d30113bf8087408e0b25c7
shreyasDarwhatkar <shreyas.darwhatkar@hotmail.com>
2015-05-23 10:50:53 -0700

    Image Added

--- src/MAHLI/MainModule.java
+++ src/MAHLI/MainModule.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class MainModule {
 
 	public static void main(String[] args) {
-		File file = new File("F:\\CS540ColorTest.jpg");
+		File file = new File("CS540ColorTest.jpg");
 		 ProcessImage objprocessImage=new ProcessImage();
 			System.out.println(objprocessImage.getImageColor(file));
 
