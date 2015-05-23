e04f775fbe52727946bce8b856c817a3a2236781
shreyasDarwhatkar <shreyas.darwhatkar@hotmail.com>
2015-05-23 11:18:01 -0700

    Image Added

--- src/MAHLI/MainModule.java
+++ src/MAHLI/MainModule.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class MainModule {
 
 	public static void main(String[] args) {
-		File file = new File("CS540ColorTest.jpg");
+		File file = new File("mahli_zinc.jpg");
 		 ProcessImage objprocessImage=new ProcessImage();
 			System.out.println(objprocessImage.getImageColor(file));
 
