db36b8b8311ab747b20616a66ea9ab8c1c2db6df
Harsh Gadhia <harshgadhia@users.noreply.github.com>
2015-05-27 14:29:36 -0700

    Module 2 commit.

--- src/telecommunications/TELMain.java
+++ src/telecommunications/TELMain.java
@@ -11,19 +11,22 @@ public class TELMain {
         System.out.println("2. TEL_POWER_OFF");
         System.out.println("3. TEL_RELAY_TO_EARTH");
         System.out.println("4. TEL_RELAY_TO_ROVER");
-        System.out.println("TEL_CHANNEL");
-        System.out.println("TEL_FREQUENCY");
-        System.out.println("TEL_BANDWIDTH");
-        System.out.println("TEL_MODULATION");
+        System.out.println("5. TEL_CHANNEL");
+        System.out.println("6. TEL_FREQUENCY");
+        System.out.println("7. TEL_BANDWIDTH");
+        System.out.println("8. TEL_MODULATION");
+        System.out.println("9. TEL_SIGNAL_RECEIVED");
+        System.out.println("10. Exit");
+        
         
         UHF t = new UHF();
+        Buffer b = new Buffer(10);
         t.printObject();
-        
         
         String data = "This is some data to be transmitted to the earth.";
         
         
-        Buffer b = new Buffer(10);
+        
         
         try {
             b.push( data );
